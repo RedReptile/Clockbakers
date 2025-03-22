fd720227242c5b34bab095ef03ee4a03115c7e2f
RedReptile <145098167+RedReptile@users.noreply.github.com>
2025-03-22 23:55:22 +0545

    SignUp Page Initialization

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,94 +1,116 @@
 import React from "react";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from '../assets/logo.png';
 
 export const LoginPage = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="w-[1269px] h-[94px] top-[25px] bg-[#efeded] shadow-[0px_4px_4px_4px_#00000040] absolute">
-                <div className="absolute w-[243px] h-[11px] top-[42px] left-10">
-                    <a href="#" className="absolute w-[55px] top-0 left-0 [font-family:'Josefin_Sans',Helvetica] font-semibold text-black text-[15px] tracking-[1.00px] leading-[normal] whitespace-nowrap">
-                        HOME
-                    </a>
-
-                    <a href="#" className="absolute w-12 top-0 left-24 [font-family:'Josefin_Sans',Helvetica] font-semibold text-black text-[15px] tracking-[1.00px] leading-[normal] whitespace-nowrap">
-                        SHOP
-                    </a>
-
-                    <a href="#" className="absolute w-[53px] top-0 left-[184px] [font-family:'Josefin_Sans',Helvetica] font-semibold text-black text-[15px] tracking-[0] leading-[normal] whitespace-nowrap">
-                        ABOUT
-                    </a>
-
-                    <img className="absolute w-[67px] h-[74px] top-[-30px] left-[550px] object-cover" alt="CLockBakers Logo" src="https://c.animaapp.com/D5ip3B6T/img/middle-logo@2x.png"></img>
-                    
-                    <img className="absolute w-[34px] h-[34px] top-[-8px] left-[900px] object-cover" alt="Cart image" src="https://c.animaapp.com/D5ip3B6T/img/cart-image@2x.png"></img>
-                    <img class="absolute w-5 h-[21px] top-[-8px] left-[925px] object-cover" alt="Cart number" src="https://c.animaapp.com/D5ip3B6T/img/cart-number@2x.png"></img>
-
-                    <div className="absolute w-[145px] top-0 left-[980px] [font-family:'Josefin_Sans-Regular',Helvetica] font-semibold text-black text-[15px] tracking-[1.00px] leading-[normal] whitespace-nowrap">
-                        LOGIN/REGISTER
+            <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-white/90 backdrop-blur-sm shadow-sm rounded-lg z-50">
+                <div className="flex items-center justify-between px-6 py-2">
+                    {/* Left Side */}
+                    <div className="flex space-x-15 mr-100 m-4">
+                        <a href="/" className="text-gray-800 hover:text-gray-600 text-sm font-medium">
+                            HOME
+                        </a>
+                        <a href="/shop" className="text-gray-800 hover:text-gray-600 text-sm font-medium">
+                            SHOP
+                        </a>
+                        <a href="/about" className="text-gray-800 hover:text-gray-600 text-sm font-medium">
+                            ABOUT
+                        </a>
                     </div>
 
-                    <div class="absolute w-[40px] h-[40px] top-[-13px] left-[1150px] bg-[url(https://c.animaapp.com/D5ip3B6T/img/image-4@2x.png)] bg-cover bg-[50%_50%]" alt="Profile"></div>
+                    {/* Clockbakers Logo */}
+                    <div className="absolute left-1/2 transform -translate-x-1/2">
+                        <img
+                            src={logo}
+                            alt="Clockbakers Logo"
+                            className="h-15"
+                        />
+                    </div>
 
+                    {/* Right Side */}
+                    <div className="flex items-center space-x-15">
+                        {/* Cart Icon */}
+                        <a href="/cart" className="text-gray-800 hover:text-gray-600">
+                            <FaShoppingCart className="text-lg" />
+                        </a>
+
+                        {/* Login/Register */}
+                        <a
+                            href="/login"
+                            className="text-gray-800 hover:text-gray-600 text-sm font-medium bg-transparent"
+                        >
+                            LOGIN/REGISTER
+                        </a>
+
+                        {/* Profile Icon */}
+                        <a href="/profile" className="text-gray-800 hover:text-gray-600">
+                            <FaUser className="text-lg" />
+                        </a>
+                    </div>
                 </div>
-            </div>
+            </nav>
 
-            <div className="absolute w-[920px] h-[650px] -mt-[-70px] bg-white shadow-[0px_2px_2px_2px_00000040] border-gray-400 border-1">
-                <div className="absolute w-[592px] top-[40px] left-[-40px] [font-family:'Tangerine-Bold'] font-Bold text-black text-4xl text-center tracking-[0] leading-[normal]">
+            <div className="absolute w-[600px] h-[500px] bg-white shadow-[0px_2px_2px_2px_00000040] border-gray-400 border-1 flex flex-col items-center justify-start pt-24">
+                <div className="absolute w-[480px] top-[40px] left-[-35px] [font-family:'Tangerine-Bold'] font-Bold text-black text-3xl text-center tracking-[0] leading-[normal]">
                     Sign Up for ClockBakers
                 </div>
-                <p className="absolute w-[269px] top-[70px] left-[75px] [font-family:'Tangerine-Bold'] font-bold text-transparent text-[17px] tracking-[0.75px] leading-[103px] whitespace-nowrap">
+                <p className="absolute w-[240px] top-[50px] left-[55px] [font-family:'Tangerine-Bold'] font-bold text-transparent text-[16px] tracking-[0.75px] leading-[103px] whitespace-nowrap">
                     <span className="text-[#050706] tracking-[0.19px]">
-                      Enjoy fresh items of our{" "}
+                        Enjoy fresh items of our{" "}
                     </span>
-
                     <span className="text-[#f74781] tracking-[0.19px]">
-                      Clock Bakers{" "}
+                        Clock Bakers{" "}
                     </span>
                 </p>
 
-                <div className="absolute w-[754px] h-[74px] top-0 left-0">
-                    <div className="relative w-full h-full -top-[-170px] -left-[-80px] border-[#838383] border-1">
+                {/* Form Section */}
+                <form className="w-[500px] space-y-4 mt-15">
+                    {/* Username Form */}
+                    <div className="relative w-full h-[60px] border-[#838383] border-1 rounded-lg">
+                        <label htmlFor="username" className="sr-only">Username</label>
                         <input 
                             id="username" 
                             type="text" 
                             placeholder="USERNAME" 
-                            className="absolute top-0 left-[23px] w-full h-full text-sm font-bold text-[#616161] [font-family:'Zen_Kaku_Gothic_Antique-Bold',Helvetica] focus:outline-none focus:text-[#616161] focus:placeholder-transparent" 
+                            className="w-full h-full text-sm font-bold text-[#616161] focus:outline-none focus:text-[#616161] focus:placeholder-transparent rounded-lg px-3 py-2"
                         />
                     </div>
-                </div>
 
-                <div className="absolute w-[754px] h-[74px] top-[100px] left-0">
-                    <div className="relative w-full h-full -top-[-170px] -left-[-80px] border-[#838383] border-1">
+                    {/* Email Form */}
+                    <div className="relative w-full h-[60px] border-[#838383] border-1 rounded-lg">
+                        <label htmlFor="email" className="sr-only">Email</label>
                         <input 
                             id="email" 
                             type="email" 
                             placeholder="EMAIL" 
-                            className="absolute top-0 left-[23px] w-full h-full text-sm font-bold text-[#616161] [font-family:'Zen_Kaku_Gothic_Antique-Bold',Helvetica] focus:outline-none focus:text-[#616161] focus:placeholder-transparent" 
+                            className="w-full h-full text-sm font-bold text-[#616161] focus:outline-none focus:text-[#616161] focus:placeholder-transparent rounded-lg px-3 py-2"
                         />
                     </div>
-                </div>
 
-                <div className="absolute w-[754px] h-[74px] top-[200px] left-0">
-                    <div className="relative w-full h-full -top-[-170px] -left-[-80px] border-[#838383] border-1">
+                    {/* Password Form */}
+                    <div className="relative w-full h-[60px] border-[#838383] border-1 rounded-lg">
+                        <label htmlFor="password" className="sr-only">Password</label>
                         <input 
                             id="password" 
                             type="password" 
                             placeholder="PASSWORD" 
-                            className="absolute top-0 left-[23px] w-full h-full text-sm font-bold text-[#616161] [font-family:'Zen_Kaku_Gothic_Antique-Bold',Helvetica] focus:outline-none focus:text-[#616161] focus:placeholder-transparent" 
+                            className="w-full h-full text-sm font-bold text-[#616161] focus:outline-none focus:text-[#616161] focus:placeholder-transparent rounded-lg px-3 py-2"
                         />
                     </div>
-                </div>
 
-                <div className="absolute w-[754px] h-[74px] top-[300px] left-0">
-                    <div className="relative w-full h-full -top-[-170px] -left-[-80px] border-[#838383] border-1">
+                    {/* Confirm Password Form */}
+                    <div className="relative w-full h-[60px] border-[#838383] border-1 rounded-lg">
+                        <label htmlFor="confirmPassword" className="sr-only">Confirm Password</label>
                         <input 
                             id="confirmPassword" 
                             type="password" 
                             placeholder="CONFIRM PASSWORD" 
-                            className="absolute top-0 left-[23px] w-full h-full text-sm font-bold text-[#616161] [font-family:'Zen_Kaku_Gothic_Antique-Bold',Helvetica] focus:outline-none focus:text-[#616161] focus:placeholder-transparent" 
+                            className="w-full h-full text-sm font-bold text-[#616161] focus:outline-none focus:text-[#616161] focus:placeholder-transparent rounded-lg px-3 py-2"
                         />
                     </div>
-                </div>
-
+                </form>
             </div>
         </div>
     );
